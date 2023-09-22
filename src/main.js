af8d6433c2234d3d8b4de041d5dda458d6479b9b
soareslssa <soareslssa.dev@gmail.com>
2023-09-22 16:43:47 -0300

    feat: add axios and vue-axios

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+createApp(App)
+.mount('#app')
+.use(VueAxios,axios)
